21f6310c12524e9c1496bc5fd978196824880432
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2015-02-16 12:58:21 -0600

    Path smoothing

--- src/cutout.js
+++ src/cutout.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var V2 = require('../lib/engine/core/vector').V2;
+var P2 = require('../lib/engine/core/vector').P2;
+var S  = require('../lib/engine/core/vector').S;
+
+
+var SMOOVE_DISTANCE = 25;
 
 //Events
 function cutoutEvents(layer){
@@ -13,24 +19,43 @@ function cutoutEvents(layer){
 
 //Controller
 function cutoutController(events){
+  events.pathDrag.onValue(function(){});
+  
   function pathString(pts){
-    return 'M' + pts.map(function(e){
-        return e.offsetX + ',' + e.offsetY;
+    return 'M' + pts.map(function(pt){
+        return pt.x + ',' + pt.y;
       })
       .join('L');
   }
+  function moveTowards(current,target){
+    var direction = V2.fromTo(current, target);
+    var magnitude = direction.magnitude();
+    
+    if(magnitude > SMOOVE_DISTANCE){
+      var d2 = direction.normalize().times(S(magnitude - SMOOVE_DISTANCE));
+      return current.offset(d2);
+    } else {
+      return current;
+    }
+  }
+  
+  var targetPoint = events
+    .pathDrag
+    .map(function(e){ return P2(e.offsetX, e.offsetY); });
 
-  events.pathDrag.onValue(function(){});
-
-  var path = events
+  var currentPoint = events
     .pathStart
-    .flatMapLatest(function(){
-      return events
-        .pathDrag
-        .scan([], '.concat')
-        .map(pathString)
-        .skip(1);
+    .map(function(e){ return P2(e.offsetX, e.offsetY); })
+    .flatMapLatest(function(pt){
+      return targetPoint
+        .scan(pt, moveTowards)
+        .skipDuplicates(P2.equals);
     });
+
+  var path = currentPoint
+    .scan([], '.concat')
+    .map(pathString)
+    .skip(1);
 
   return {
     path:    path,
